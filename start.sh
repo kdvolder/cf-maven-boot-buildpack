7a4a9b470f87ce80cf8b2eae92ca8e181d43a9ba
Kris De Volder <kdevolder@pivotal.io>
2016-08-16 15:50:56 -0700

    Fix launch script

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ echo KDV_TEST=$KDV_TEST
 echo VCAP_APPLICATION=$VCAP_APPLICATION
 
 echo STARTING JAVA APP
-$JAVA_HOME/bin/java -cp unpacked org.springframework.boot.loader.JarLauncher
+$JAVA_HOME/bin/java -cp ${HOME}/classpath org.springframework.boot.loader.JarLauncher
