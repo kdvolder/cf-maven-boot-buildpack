b5854ba440b169a83b18fef6ab86e0a14d56b864
Kris De Volder <kdevolder@pivotal.io>
2016-08-16 15:13:07 -0700

    Unpack app jar into 'classpath' folder.

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ echo KDV_TEST=$KDV_TEST
 echo VCAP_APPLICATION=$VCAP_APPLICATION
 
 echo STARTING JAVA APP
-$JAVA_HOME/bin/java -jar target/*.jar
+$JAVA_HOME/bin/java -cp unpacked org.springframework.boot.loader.JarLauncher
