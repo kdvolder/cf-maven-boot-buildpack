353f4b9a83ce158f50557352ba9d822735540022
Kris De Volder <kdevolder@pivotal.io>
2016-08-17 14:42:23 -0700

    Orion should run on port 9000

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ echo VCAP_APPLICATION=$VCAP_APPLICATION
 
 echo STARTING ORION
 export PATH=${PATH}:${HOME}/nodejs/node-v4.5.0-linux-x64/bin:${HOME}/node_modules/.bin
-orion -p 54321 &
+orion -p 9000 &
 
 echo STARTING JAVA APP
 $JAVA_HOME/bin/java -cp ${HOME}/classpath org.springframework.boot.loader.JarLauncher
