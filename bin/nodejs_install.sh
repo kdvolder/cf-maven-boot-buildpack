c2d8db32daef6fc219367bf3db8be0c71c6fec74
Kris De Volder <kdevolder@pivotal.io>
2016-08-17 13:07:48 -0700

    Installl orion inside the app, not globally

--- bin/nodejs_install.sh
+++ bin/nodejs_install.sh
@@ -27,6 +27,7 @@ echo "Node installation directory" `pwd`
 echo "Node version: " `./bin/node -v`
 echo "NPM version: " `./bin/npm -v`
 
+cd appDir
 mkdir node_modules
 echo "Installing Orion via npm"
 ./bin/npm install orion
