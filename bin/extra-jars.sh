cd3325913839deaf60581567d8cfb95dd52d36ff
BoykoAlex <aboyko@pivotal.io>
2016-08-18 12:48:08 -0400

    Add zuul

--- bin/extra-jars.sh
+++ bin/extra-jars.sh
@@ -6,10 +6,12 @@ classpathDir=${appDir}/classpath/BOOT-INF/lib
 ideVersion=0.0.3
 devtoolsVersion=1.4.0.RELEASE
 collectionsVersion=3.2.2
+zuulVersion=1.1.5.RELEASE
 
 ideUrl=http://aboyko-ide-jar-server.cfapps.io/ide/ide-${ideVersion}.jar
 devtoolsUrl=http://central.maven.org/maven2/org/springframework/boot/spring-boot-devtools/${devtoolsVersion}/spring-boot-devtools-${devtoolsVersion}.jar
 collectionsUrl=http://central.maven.org/maven2/commons-collections/commons-collections/${collectionsVersion}/commons-collections-${collectionsVersion}.jar
+zuulUrl=http://central.maven.org/maven2/org/springframework/cloud/spring-cloud-starter-zuul/${zuulVersion}/spring-cloud-starter-zuul-${zuulVersion}.jar
 
 ##############################################################################################
 # Add IDE JAR
@@ -32,9 +34,9 @@ cp -v ide-${ideVersion}.jar ${classpathDir}/ide-${ideVersion}.jar
 echo "===== Adding DevTools JAR ====="
 cd $cacheDir
 if [ -f spring-boot-devtools-${devtoolsVersion}.jar ]; then
-echo "Found spring-boot-devtools.jar in cache"
+    echo "Found spring-boot-devtools.jar in cache"
 else
-wget $devtoolsUrl -O spring-boot-devtools-${devtoolsVersion}.jar
+    wget $devtoolsUrl -O spring-boot-devtools-${devtoolsVersion}.jar
 fi
 
 cp -v spring-boot-devtools-${devtoolsVersion}.jar ${classpathDir}/spring-boot-devtools-${devtoolsVersion}.jar
@@ -52,4 +54,18 @@ else
 fi
 
 cp -v commons-collections-${collectionsVersion}.jar ${classpathDir}/commons-collections-${collectionsVersion}.jar
+
+##############################################################################################
+# Add Collections Jar
+##############################################################################################
+
+echo "===== Adding Zuul JAR ====="
+cd $cacheDir
+if [ -f spring-cloud-starter-zuul-${zuulVersion}.jar ]; then
+    echo "Found spring-cloud-starter-zuul-${zuulVersion}.jar in cache"
+else
+    wget $devtoolsUrl -O spring-cloud-starter-zuul-${zuulVersion}.jar
+fi
+
+cp -v spring-cloud-starter-zuul-${zuulVersion}.jar ${classpathDir}/spring-cloud-starter-zuul-${zuulVersion}.jar
 
