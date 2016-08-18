a725f2734e2b9ba35a0e19b3687a9792da92753a
BoykoAlex <aboyko@pivotal.io>
2016-08-18 13:17:03 -0400

    Function for adding a jar on the classpath

--- bin/extra-jars.sh
+++ bin/extra-jars.sh
@@ -13,47 +13,64 @@ devtoolsUrl=http://central.maven.org/maven2/org/springframework/boot/spring-boot
 collectionsUrl=http://central.maven.org/maven2/commons-collections/commons-collections/${collectionsVersion}/commons-collections-${collectionsVersion}.jar
 zuulUrl=http://central.maven.org/maven2/com/netflix/zuul/zuul-core/${zuulVersion}/zuul-core-${zuulVersion}.jar
 
+addJar() {
+    local file = $1
+    local url = $2
+    local message = $3
+    echo ${message}
+    cd $cacheDir
+    if [ -f ${file} ]; then
+        echo "Found ${file} in cache"
+    else
+        wget $url -O $file
+    fi
+    cp -v $file ${classpathDir}/${file}
+}
+
 ##############################################################################################
 # Add IDE JAR
 ##############################################################################################
 
-echo "===== Adding IDE JAR ====="
-cd $cacheDir
-if [ -f ide-${ideVersion}.jar ]; then
-    echo "Found ide-${ideVersion}.jar in cache"
-else
-    wget $ideUrl -O ide-${ideVersion}.jar
-fi
-
-cp -v ide-${ideVersion}.jar ${classpathDir}/ide-${ideVersion}.jar
+addJar $ideUrl ide-${ideVersion}.jar "===== Adding IDE JAR ====="
+#echo "===== Adding IDE JAR ====="
+#cd $cacheDir
+#if [ -f ide-${ideVersion}.jar ]; then
+#    echo "Found ide-${ideVersion}.jar in cache"
+#else
+#    wget $ideUrl -O ide-${ideVersion}.jar
+#fi
+#
+#cp -v ide-${ideVersion}.jar ${classpathDir}/ide-${ideVersion}.jar
 
 ##############################################################################################
 # Add DevTools Jar
 ##############################################################################################
 
-echo "===== Adding DevTools JAR ====="
-cd $cacheDir
-if [ -f spring-boot-devtools-${devtoolsVersion}.jar ]; then
-    echo "Found spring-boot-devtools.jar in cache"
-else
-    wget $devtoolsUrl -O spring-boot-devtools-${devtoolsVersion}.jar
-fi
-
-cp -v spring-boot-devtools-${devtoolsVersion}.jar ${classpathDir}/spring-boot-devtools-${devtoolsVersion}.jar
+addJar $devtoolsUrl spring-boot-devtools-${devtoolsVersion}.jar "===== Adding DevTools JAR ====="
+#echo "===== Adding DevTools JAR ====="
+#cd $cacheDir
+#if [ -f spring-boot-devtools-${devtoolsVersion}.jar ]; then
+#    echo "Found spring-boot-devtools.jar in cache"
+#else
+#    wget $devtoolsUrl -O spring-boot-devtools-${devtoolsVersion}.jar
+#fi
+#
+#cp -v spring-boot-devtools-${devtoolsVersion}.jar ${classpathDir}/spring-boot-devtools-${devtoolsVersion}.jar
 
 ##############################################################################################
 # Add Collections Jar
 ##############################################################################################
 
-echo "===== Adding Collections JAR ====="
-cd $cacheDir
-if [ -f commons-collections-${collectionsVersion}.jar ]; then
-    echo "Found spring-boot-devtools.jar in cache"
-else
-    wget $devtoolsUrl -O commons-collections-${collectionsVersion}.jar
-fi
-
-cp -v commons-collections-${collectionsVersion}.jar ${classpathDir}/commons-collections-${collectionsVersion}.jar
+addJar $devtoolsUrl commons-collections-${collectionsVersion}.jar "===== Adding Collections JAR ====="
+#echo "===== Adding Collections JAR ====="
+#cd $cacheDir
+#if [ -f commons-collections-${collectionsVersion}.jar ]; then
+#    echo "Found spring-boot-devtools.jar in cache"
+#else
+#    wget $devtoolsUrl -O commons-collections-${collectionsVersion}.jar
+#fi
+#
+#cp -v commons-collections-${collectionsVersion}.jar ${classpathDir}/commons-collections-${collectionsVersion}.jar
 
 ##############################################################################################
 # Add Zuul Jar
@@ -68,4 +85,3 @@ cp -v commons-collections-${collectionsVersion}.jar ${classpathDir}/commons-coll
 #fi
 #
 #cp -v zuul-core-${zuulVersion}.jar ${classpathDir}/zuul-core-${zuulVersion}.jar
-
